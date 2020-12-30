dd1f918addbc80e7546117e3935e6a05b9f91dea
Zi Wang <ziwang55@gmail.com>
2020-12-29 19:05:28 -0600

    sql role inserted

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
---Creating our database--
+-- Creating our database
 DROP DATABASE IF EXISTS employee_trackerDB;
 
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
---Department table--
+-- Department table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
---Role table--
+-- Role table
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title CARCHAR(30),
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
---Employee role table--
+-- Employee role table
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
@@ -30,7 +30,7 @@ CREATE TABLE employee (
 );
 
 
---Department seeds--
+-- Department seeds
 INSERT INTO department (name)
 VALUE ("Sales");
 INSERT INTO department (name)
@@ -39,5 +39,27 @@ INSERT INTO department (name)
 VALUE ("Finance");
 INSERT INTO department (name)
 VALUE ("Legal");
+
+-- Employee role seeds
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 8000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+
+
+-- Selecting
+SELECT * FROM department;
+SELECT * FROM role;
 
 
