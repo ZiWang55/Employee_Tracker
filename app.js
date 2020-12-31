413fb90d56bbb9a5b5ba07f1880bad8039d9718e
zi wang <ziwang55@gmail.com>
2020-12-30 22:23:21 -0600

    tested view all employees function works

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
 //Dependencies
-const inquirer = require("inquirer")
-const mysql = require("mysql")
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 const cTable = require("console.table");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "fireman",
-    database: "employee_trackerDB"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "fireman",
+  database: "employee_trackerDB",
 });
 
 //Connection id
-connection.connect
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
+  startPrompt();
+});
+
+//Initial Prompt
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees?",
+          "View All Employees By Roles?",
+          "View All Emplyees By Department?",
+          "Update Employee",
+          "Add Employee?",
+          "Add Role?",
+          "Add Department?",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees?":
+          viewAllEmployees();
+          break;
+      }
+    });
+}
+
+//View all emplyees
+function viewAllEmployees() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
+}
+//View all roles
+
+//View all employees by departments
+
+//Add  emplyee prompt
+
+//
--- schema.sql
+++ schema.sql
@@ -6,100 +6,78 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 -- DEPARTMENT TABLE ----
-CREATE TABLE department
-(
-    id INT NOT NULL
-    AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR
-    (30)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30)
 );
 
-    -- DEPARTMENT TABLE ----
-    CREATE TABLE role
-    (
-        id INT NOT NULL
-        AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR
-        (30),
+-- DEPARTMENT TABLE ----
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY
-        (department_id) REFERENCES department
-        (id)
+    FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-        -- EMPLOYEE ROLE TABLE ----
-        CREATE TABLE employee
-        (
-            id INT NOT NULL
-            AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR
-            (30),
-    last_name VARCHAR
-            (30),
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
     manager_id INT,
     role_id INT,
-    FOREIGN KEY
-            (role_id) REFERENCES role
-            (id),
-    FOREIGN KEY
-            (manager_id) REFERENCES employee
-            (id)
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
 
-            -- Department seeds
-            INSERT INTO
-    department
-                (name)
-            VALUE
-            ("Sales"),
-            ("Engineering"),
-            ("Finance"),
-            ("Legal");
+-- Department seeds
+INSERT INTO
+    department (name) VALUE ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
 
-            -- Employee role seeds
-            INSERT INTO
-    role
-                (title, salary, department_id)
-            VALUE
-            ("Lead Engineer",
-            150000,
-            2
-            ),
-            ("Legal Team Lead", 250000, 4),
-            ("Accountant", 125000, 3),
-            ("Sales Lead", 100000, 1),
-            ("Salesperson", 8000, 1),
-            ("Software Engineer", 120000, 2),
-            ("Lawyer", 190000, 4);
+-- Employee role seeds
+INSERT INTO
+    role (title, salary, department_id) VALUE (
+        "Lead Engineer",
+        150000,
+        2
+    ),
+    ("Legal Team Lead", 250000, 4),
+    ("Accountant", 125000, 3),
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 8000, 1),
+    ("Software Engineer", 120000, 2),
+    ("Lawyer", 190000, 4);
 
-            -- Employee seeds
-            INSERT INTO
-    employee
-                (first_name, last_name, manager_id, role_id)
-            VALUE("Jeffery",
-            "Hoseman",
-            null,
-            1
-            ),
-            ("Jason", "Lao", null, 3),
-            ("Tiff", "Melby", 1, 4),
-            ("Mia", "Lam", null, 2),
-            ("Mercedes", "Johnson", 4, 5),
-            ("Jack", "Wong", 2, 7);
+-- Employee seeds
+INSERT INTO
+    employee (first_name, last_name, manager_id, role_id) VALUE(
+        "Jeffery",
+        "Hoseman",
+        null,
+        1
+    ),
+    ("Jason", "Lao", null, 3),
+    ("Tiff", "Melby", 1, 4),
+    ("Mia", "Lam", null, 2),
+    ("Mercedes", "Johnson", 4, 5),
+    ("Jack", "Wong", 2, 7);
 
-            -- Selecting
-            SELECT
-                *
-            FROM
-                department;
+-- Selecting
+SELECT
+    *
+FROM
+    department;
 
-            SELECT
-                *
-            FROM
-                role;
+SELECT
+    *
+FROM
+    role;
 
-            SELECT
-                *
-            FROM
-                employee;
+SELECT
+    *
+FROM
+    employee;
