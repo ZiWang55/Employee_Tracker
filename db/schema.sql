7993e3b1de434f8da283a61e368d8d1b057f8664
zi wang <ziwang55@gmail.com>
2020-12-30 21:35:35 -0600

    change to sql tables made

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,34 @@
--- Creating our database
-DROP DATABASE IF EXISTS employees;
+--CREATING OUR DATABASE --
+DROP DATABASE IF EXISTS employee_trackerDB;
 
-CREATE DATABASE employees;
+CREATE DATABASE employee_trackerDB;
 
-USE employees;
+USE employee_trackerDB;
 
--- Department table
+-- DEPARTMENT TABLE ----
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
 );
--- Role table
+-- DEPARTMENT TABLE ----
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INT
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
--- Employee role table
+-- EMPLOYEE ROLE TABLE ----
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
 
 
